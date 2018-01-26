b2df6caa5018820d8db662e8e5ff272b2e06fe18
tapansirole <tapansirole@gmail.com>
2018-01-26 10:56:21 +0530

    first commit

---
+++ src/static_import/Example1.java
@@ -0,0 +1,12 @@
+package static_import;
+
+import static java.lang.System.out;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		out.print("My first static import example.....");
+
+	}
+
+}
